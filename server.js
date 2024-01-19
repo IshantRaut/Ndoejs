55255811212ef60efa304404e444d3bc7bc493ad
IshantRaut <ishjee2018@gmail.com>
2024-01-19 15:07:36 +0530

    Create object

--- server.js
+++ server.js
@@ -10,3 +10,22 @@ const Student={
     College:'KITS college'
 }
 console.log(Student);
+
+let array=['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+let newarray=array.map(item=>item===' '?'empty string':item);
+
+const copyarray=[...array];
+console.log(copyarray);
+
+console.log(newarray);
+
+
+const obj1 = {'key1': 1 , 'key2' : 2}
+
+const obj2 = { ...obj1, key1: 1000}
+
+
+
+console.log(obj1)
+
+console.log(obj2)
